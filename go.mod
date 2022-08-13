3421d49db1eab62498fd5e49601ee62a6866dae6
Arowolo Abiodun <aarowolom@gmail.com>
2022-08-13 21:38:48 +0100

    new change

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/aro-wolo/ases
 
 go 1.18
 
-require (
-	github.com/aws/aws-sdk-go v1.44.75 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-)
+require gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+
+require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
--- main.go
+++ main.go
@@ -1,87 +1,47 @@
 package ases
 
 import (
-	"fmt"
+	"crypto/tls"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ses"
-)
-
-const (
-	CharSet = "UTF-8"
+	"gopkg.in/gomail.v2"
 )
 
 type Header struct {
-	Region   string
-	Cc       []*string
-	To       []*string
-	HtmlBody string
-	Subject  string
+	Username string
 	From     string
+	Password string
+	Host     string
+	To       string
+	Subject  string
 }
 
-func Send(h Header) {
+func Sendmail(h Header, msg string) {
 
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(h.Region)},
-	)
+	m := gomail.NewMessage()
+	m.SetHeader("From", h.From)
+	m.SetHeader("To", h.To)
+	m.SetHeader("Subject", h.Subject)
+	m.SetBody("text/html", msg)
 
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	d := gomail.NewDialer(h.Host, 587, h.Username, h.Password)
+	d.DialAndSend(m)
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: false}
 
-	// Create an SES session.
-	svc := ses.New(sess)
-
-	// Assemble the email.
-	input := &ses.SendEmailInput{
-		Destination: &ses.Destination{
-			CcAddresses: h.Cc,
-			ToAddresses: h.To,
-		},
-		Message: &ses.Message{
-			Body: &ses.Body{
-				Html: &ses.Content{
-					Charset: aws.String(CharSet),
-					Data:    aws.String(h.HtmlBody),
-				},
-			},
-			Subject: &ses.Content{
-				Charset: aws.String(CharSet),
-				Data:    aws.String(h.Subject),
-			},
-		},
-		Source: aws.String(h.From),
-	}
-
-	// Attempt to send the email.
-	result, err := svc.SendEmail(input)
-
-	// Display error messages if they occur.
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case ses.ErrCodeMessageRejected:
-				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
-			case ses.ErrCodeMailFromDomainNotVerifiedException:
-				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
-			case ses.ErrCodeConfigurationSetDoesNotExistException:
-				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-
-		return
-	}
-
-	fmt.Print("Email Sent to address: ")
-	fmt.Println(h.To)
-	fmt.Println(result)
 }
+
+/* func main() {
+
+	h := Header{}
+	h.Username = "AKIAIE6URQG7YVNUOSIA"
+	h.Password = "AlH8zkhRZh3rs+G3wDa4pWkJ3NOzjHi7a5tos9Hpr8nl"
+	h.Host = "email-smtp.us-west-2.amazonaws.com"
+	h.From = "do-not-reply@officelime.app"
+	h.Subject = "Testing"
+	h.To = "adexfe@live.com"
+
+	Sendmail(h, "Hello world")
+
+	println("hi...")
+
+}
+*/
