63716e4add61326af7ba216f498919c73d4c14ca
Arowolo Abiodun <aarowolom@gmail.com>
2022-08-14 11:46:59 +0100

    remove key

--- main.go
+++ main.go
@@ -11,16 +11,14 @@ type Header struct {
 	From     string
 	Password string
 	Host     string
-	To       string
-	Subject  string
 }
 
-func Sendmail(h Header, msg string) {
+func Sendmail(to string, sub string, msg string, h Header) {
 
 	m := gomail.NewMessage()
 	m.SetHeader("From", h.From)
-	m.SetHeader("To", h.To)
-	m.SetHeader("Subject", h.Subject)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", sub)
 	m.SetBody("text/html", msg)
 
 	d := gomail.NewDialer(h.Host, 587, h.Username, h.Password)
@@ -28,20 +26,3 @@ func Sendmail(h Header, msg string) {
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: false}
 
 }
-
-/* func main() {
-
-	h := Header{}
-	h.Username = "AKIAIE6URQG7YVNUOSIA"
-	h.Password = "AlH8zkhRZh3rs+G3wDa4pWkJ3NOzjHi7a5tos9Hpr8nl"
-	h.Host = "email-smtp.us-west-2.amazonaws.com"
-	h.From = "do-not-reply@officelime.app"
-	h.Subject = "Testing"
-	h.To = "adexfe@live.com"
-
-	Sendmail(h, "Hello world")
-
-	println("hi...")
-
-}
-*/
